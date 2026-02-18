d3b97d63028567c7f005f977ae7bd3ad9a8cff86
Juanin_999 <Kall.su1515@hotmail.com>
2026-02-17 19:44:23 -0500

    Update: Agregar los 9 puestos de trabajo correctos

--- database/seeders/PuestoTrabajoSeeder.php
+++ database/seeders/PuestoTrabajoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PuestoTrabajo;
 
 class PuestoTrabajoSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class PuestoTrabajoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $puestos = [
+            ['nombre' => 'Primera par', 'descripcion' => 'Primera línea par'],
+            ['nombre' => 'Primera impar', 'descripcion' => 'Primera línea impar'],
+            ['nombre' => 'Segunda par', 'descripcion' => 'Segunda línea par'],
+            ['nombre' => 'Segunda impar', 'descripcion' => 'Segunda línea impar'],
+            ['nombre' => 'Zapata Izquierda', 'descripcion' => 'Zapata lado izquierdo'],
+            ['nombre' => 'Zapata Derecha', 'descripcion' => 'Zapata lado derecho'],
+            ['nombre' => 'Cadera 1', 'descripcion' => 'Cadera puesto 1'],
+            ['nombre' => 'Cadera 2', 'descripcion' => 'Cadera puesto 2'],
+            ['nombre' => 'Limpieza superior', 'descripcion' => 'Limpieza parte superior'],
+        ];
+
+        foreach ($puestos as $puesto) {
+            PuestoTrabajo::create($puesto);
+        }
     }
 }
