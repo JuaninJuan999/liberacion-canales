fde64a6f7e906ddb1f38e70ad4e0387ed6df8cbb
juan <kall.su999@gmail.com>
2026-04-29 19:33:22 -0500

    Avance

--- database/seeders/PuestoTrabajoSeeder.php
+++ database/seeders/PuestoTrabajoSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\PuestoTrabajo;
+use Illuminate\Database\Seeder;
 
 class PuestoTrabajoSeeder extends Seeder
 {
@@ -23,6 +22,16 @@ class PuestoTrabajoSeeder extends Seeder
             ['nombre' => 'Cadera 1', 'descripcion' => 'Cadera puesto 1', 'orden' => 7],
             ['nombre' => 'Cadera 2', 'descripcion' => 'Cadera puesto 2', 'orden' => 8],
             ['nombre' => 'Limpieza superior', 'descripcion' => 'Limpieza parte superior', 'orden' => 9],
+            // Tolerancia cero / enlace con ubicaciones (ver RelacionarUbicacionesPuestosSeeder y migration link_ubicaciones)
+            ['nombre' => 'Clipado de Esófago', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 10],
+            ['nombre' => 'Eviscerado de Blancas', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 11],
+            ['nombre' => 'Corte Esternón', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 12],
+            ['nombre' => 'Desuello de Pierna', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 13],
+            ['nombre' => 'Despeje de Recto', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 14],
+            ['nombre' => 'Corte de Manos', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 15],
+            ['nombre' => 'Desuello de Manos', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 16],
+            ['nombre' => 'Desolladora', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 17],
+            ['nombre' => 'Transferencia', 'descripcion' => 'Puesto tolerancia cero / ubicaciones', 'orden' => 18],
         ];
 
         foreach ($puestos as $puesto) {
@@ -30,7 +39,7 @@ class PuestoTrabajoSeeder extends Seeder
                 ['nombre' => $puesto['nombre']],
                 [
                     'descripcion' => $puesto['descripcion'],
-                    'orden' => $puesto['orden']
+                    'orden' => $puesto['orden'],
                 ]
             );
         }
